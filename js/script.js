326ee71c8bad558cf5b7234b1b4edefdb1fa93dd
Soy Fer <fernandosantucho1@gmail.com>
2024-04-19 17:50:07 -0300

    primer commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// se utiliza la funcion const para guardar varias variables 
+
+
+const numeros= [3,45,22,48,53,23,20,17,51,8]
+
+    for(n of numeros){
+        if (n%2 == 0){
+            console.log(n, "Es par")
+        }else{
+            console.log(n, "Es impar")
+        }   
+    }
+
+// funciones tiene la estrategia de dibide y reinaras, para dividir los problemas. metodos es para la POO tiene la misma teoria que las funciones
+// estructura de funcion se necesita llamar a las funcionesfunction
